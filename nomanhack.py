b4c743019d3c46b9a6c4d283669f6619ee777d85
nomankarim8 <karimnoman97@gmail.com>
2025-05-13 19:19:48 -0800

    update nomanhack.py

--- nomanhack.py
+++ nomanhack.py
@@ -1132,6 +1132,7 @@ if __name__ == '__main__':
         action='store_true',
         help='Run in a loop'
     )
+    
     parser.add_argument(
         '-r', '--reverse-scan',
         action='store_true',
