cca34ca75ccdf56ff041d128956b33381932522d
nomankarim8 <karimnoman97@gmail.com>
2025-06-30 20:26:34 +0600

    update nomanhack.py

--- nomanhack.py
+++ nomanhack.py
@@ -407,6 +407,7 @@ class BruteforceStatus:
         self.__init__()
 
 
+
 class Companion:
     """Main application part"""
     def __init__(self, interface, save_result=False, print_debug=False):
